d4e24ad459f6d24bd65199e080ccff9d98138123
Michael Brunner <michael.brunner@sm0x.org>
2020-10-21 15:49:17 +0200

    Registration deactivated

--- src/main/java/org/sm0x/tools/opencellarbook/config/SecurityConfiguration.java
+++ src/main/java/org/sm0x/tools/opencellarbook/config/SecurityConfiguration.java
@@ -78,7 +78,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .authorizeRequests()
             .antMatchers("/api/authenticate").permitAll()
-            .antMatchers("/api/register").permitAll()
+            .antMatchers("/api/register").denyAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
